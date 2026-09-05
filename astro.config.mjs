fadb7e6fdd9b1fe62969c29439557a65e129f1bb
DeepakSinghN <dt7252058269@gmail.com>
2026-09-05 12:31:44 +0530

    feat: setup official @astrojs/sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,11 +2,19 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://caraoucoroavirtual.com',
+
   vite: {
     plugins: [tailwindcss()],
   },
-});
 
+  integrations: [
+    sitemap({
+      filter: (page) => !page.includes('/404') && !page.includes('/500'),
+    }),
+  ],
+});
